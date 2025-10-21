6f37ec996fc52aaa2dc235662988fee681c798b1
andikaramadanii <andikaramadani2409@gmail.com>
2025-10-21 13:24:07 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. pilih tombol yang sesuai
+cons tombolSapa = document.getElementById('sapaButton');
+// 2. tambahkan 'event' aksi 'click'
+tombolSapa.addEventListener('click', function() {
+  // 3. jalankan kode ini
+                            alert('hai'! Terima kasih sudah berkunjung!');
+});
